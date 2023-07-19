1f4abc2e50aa234ab086447eaf475ba50e717cb8
febri <fitrafilthmongers@gmail.com>
2023-07-19 13:05:35 +0700

    tugas 19 js
    mengumpulkan tugas 19 js

---
+++ tugas19.js
@@ -0,0 +1,19 @@
+var kata = "semangat..."
+console.log(kata.repeat(5))
+
+
+var bintang = "*****"
+console.log(bintang)
+var bintang2 = bintang.repeat(4)
+console.log(bintang2)
+var bintang3 =bintang2.repeat(2)
+console.log(bintang3)
+var bintang4 = bintang2.repeat(3)
+console.log(bintang4)
+var bintang5 = bintang2.repeat(4)
+console.log(bintang5)
+console.log(bintang4)
+console.log(bintang3)
+console.log(bintang2)
+console.log(bintang)
+
